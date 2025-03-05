a51f1d3f5d84fbb8cc448198f54186ec502751ee
AlbartFox <albart24200012@gmail.com>
2025-03-04 22:29:52 -0600

    Hola

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -13,6 +13,7 @@ class LoginController extends Controller
     {
         return view('auth.login');
     }
+    
     public function login(Request $request)
     {
         $credentials = $request->only('correo', 'contrasena');
